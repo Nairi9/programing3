a56fa71f30a0f1f49bd15664cd86500da8f43652
Nairi9 <tsaghik.m@tumo.org>
2023-10-14 13:39:41 +0400

    uxxaki commit

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require("express");
+
+var app = express();
+
+app.use(express.static("."));
+
+app.get("/", function(req, res){
+
+res.redirect("index.html");
+
+});
+
+app.listen(3000, function(){
+
+console.log("Example is running on port 3000");
+
+});
